3c76f126604860dfedd2be175f252f25df69c94b
AliksBogdanovs <90691932+AliksBogdanovs@users.noreply.github.com>
2023-01-23 11:55:26 +0200

    Create 23.01.py

---
+++ 23.01.py
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Čūska</title>
+        <link href="style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="wrapper">
+            <h1>Autors: Aliks </h1>
+            <h1>Spēle "Čūska", veidota Programmēšana I priekšmeta ietvaros</h1>
+            
+        </div>
+        </div>
+    </body>
+</html>
